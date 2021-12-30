141b12e1f63e05469be0948805b41223724ee610
Dawid <dawidkwasowski@gmail.com>
2021-12-30 17:13:00 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="container__banner">
+      <!-- image -->
+    </div>
+    <main class="container__main-content">
+      <!-- filtered area -->
+      <section class="chips-container">
+        <!-- here will be placed a chips components  -->
+      </section>
+      <section class="offers-container">
+        <!-- here will be placed a job offers -->
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  // single source of truth
+  data() {
+    return {
+      offers: []
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
